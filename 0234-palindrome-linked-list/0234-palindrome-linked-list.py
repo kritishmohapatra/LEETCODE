4d5d583c79dc1a708b2e92f25f48298e08602e05
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2025-08-14 22:23:54 +0530

    Time: 91 ms (5.07%), Space: 43.9 MB (5.11%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverse(self, head):
+        if head is None or head.next is None:
+            return head
+        new_head=self.reverse(head.next)
+        front=head.next
+        front.next=head
+        head.next=None
+        return new_head
+
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if head is None or head.next is None:
+            return True
+        slow=head
+        fast=head
+        while fast.next is not None  and fast.next.next is not None:
+            slow=slow.next
+            fast=fast.next.next
+        new_head=self.reverse(slow.next)
+        first=head
+        second=new_head
+        while second is not None:
+            if first.val!=second.val:
+                self.reverse(new_head)
+                return False
+            first=first.next
+            second=second.next
+        self.reverse(new_head)
+        return True
+
+        
